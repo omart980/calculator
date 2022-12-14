f93c427b6c6ca2f636fd48f57427f49c8dbf0365
Omar Torres <omartorres980.com>
2022-12-13 20:57:59 -0800

    Interactive with buttons, click

--- calculator.js
+++ calculator.js
@@ -1,3 +1,10 @@
+let screen = document.getElementById('screen');// save this as a reference, not as a class in html
+
+// inside () collects as NodeList, but convert to array of buttons
+let buttons = Array.from(document.getElementsByClassName('button')); 
+
+/*Calculator functions*/
+/*
 function add(x, y){
     return x + y;
 }
@@ -13,29 +20,57 @@ function multiply(x, y){
 function divide(x, y){
     return x / y;
 }
+*/
+/*Calls appropiate functionn above*/
 
-
-//let adds = multiply(5, 7);
-//console.log(adds);
-
-function operate(operator, x, y){
-    if(operator == 'add'){
+function operate(operation) {
+    return new Function('return ' + operation)();
+}
+/*
+function operate(x, operator, y){
+    if(operator == '+'){
         return add(x,y);
     }
-    else if(operator == 'subtract'){
+    else if(operator == '-'){
         return subtract(x,y);
     }
-    else if(operator == 'multiply'){
+    else if(operator == '*'){
         return multiply(x,y);
     }
-   else if(operator == 'divide'){
+   else if(operator == '/'){
         return divide(x,y);
     }
     else {
         return "Invalid operator. Try again";
     }
 }
+*/
+//let operation = operate('divide', 9, 2);
+//console.log(operation);
 
-let operation = operate('divide', 9, 2);
-console.log(operation);
-
+buttons.map( button => { // for list of arrays
+    button.addEventListener('click', (e) => { //add a click
+        switch(e.target.innerText){ //give back the label of the clicked button
+            // inner Text gets text of an element
+            //switch gives it cases for the button switching
+            case 'C':
+                screen.innerText = '';
+                break;
+            case '=':
+                try{
+                    screen.innerText = operate(screen.innerText);
+                    console.log(typeof(screen.innerText))
+                } catch {
+                    screen.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (screen.innerText){
+                   screen.innerText = screen.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                screen.innerText += e.target.innerText; //append buttons
+        }
+    });
+});
